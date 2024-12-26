282ba6c463016d671c70cb9a63b3a67697fd9331
mizuki0220 <em9220me.i@gmail.com>
2024-12-26 15:23:24 +0000

    ユーザー側　ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/create'
+    get 'cart_items/destroy'
+  end
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
@@ -11,19 +17,30 @@ Rails.application.routes.draw do
     root to: 'homes#top'
 
     get 'home/about' => 'homes#about', as: 'about'
-    get 'customers/:id' => 'customer#show', as: 'my_page'
 
     devise_for :customers, skip: [:passwords], controllers: {
       registrations: "public/registrations",
       sessions: "public/sessions"
     }
-
+    resources :customers, only: [:show, :edit, :update]
+    get '/customers/my_page', to: 'customers#show'
+    get '/customers/information/edit', to: 'customers#edit'
+    get '/customers/information', to: 'customers#update'
     resources :customers do
       member do
         patch 'withdraw'
         get 'unsubscribe'
       end
     end
+
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+    resources :cart_items do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+
+    resources :items, only: [:index, :show]
 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_12_22_135550) do
+ActiveRecord::Schema.define(version: 2024_12_26_143747) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2024_12_22_135550) do
     t.string "telephone_number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "is_active", default: false
+    t.boolean "is_active", default: true
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
