6aaeeba840ca10e2f8a0d1cb70e0745df0007faf
mizuki0220 <em9220me.i@gmail.com>
2025-01-25 16:26:45 +0000

    cart_item/index実装中

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,6 +3,11 @@ class CartItem < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   belongs_to :customer
+  has_many :items, dependent: :destroy
+
+  def subtotal
+    item.with_tax_price * amount
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :genre
+  belongs_to :cart_item
 
   has_one_attached :image
-
+  
+  #画像が存在しない時のダミー画像表示
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -11,13 +13,15 @@ class Item < ApplicationRecord
     image
   end
 
-  def  add_tax_price
+  #税込の表示
+  def add_tax_price
     (self.price * 1.10).round
   end
   
+  #検索
   def self.search(search)
     return Item.all unless search
     Item.where(['name LIKE ? OR introduction', "%#{search}%"])
   end
-  
+
 end
