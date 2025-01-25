e17789be689b9d771c1103f0368213a0fd7c9c54
mizuki0220 <em9220me.i@gmail.com>
2025-01-25 15:18:54 +0000

    addressページ完了

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,10 +5,12 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     if @address.save
       flash[:notice] = "新しい配送先の登録が完了しました。"
       redirect_to addresses_path
@@ -20,9 +22,21 @@ class Public::AddressesController < ApplicationController
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] = "編集が完了しました。"
+      redirect_to addresses_path
+    else
+      flash[:alert] = "登録に失敗しました。"
+      render :edit
+    end
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+
   end
 
   private
