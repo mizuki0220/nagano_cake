b3057a67447a4225689fb5da60ffdc86455c9f6b
mizuki0220 <em9220me.i@gmail.com>
2025-02-06 15:47:54 +0000

    public/orders/new 修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,8 +10,22 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      flash[:notice] = "正常に登録しました"
+      redirect_to confirm_orders_path
+    else
+      @orders = Order.all
+      flash[:alert] = "登録に失敗しました"
+      render :new
+    end
   end
-  
+
   def confirm
-  end 
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :name, :post_code, :address, :shipping_fee, :total_price, :pay_method, :is_active)
+  end
 end
