869b4cc28ddd16f2827fca75ca0f7f4e2e044f40
mizuki0220 <em9220me.i@gmail.com>
2025-03-08 15:24:12 +0000

    customerマイページ　実装中

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,6 +1,7 @@
 class Public::CustomersController < ApplicationController
 
   def show
+    @customer = current_customer
   end
 
   def edit
@@ -16,5 +17,11 @@ class Public::CustomersController < ApplicationController
     flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
   end
+
+  private
+
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_active)
+    end
 
 end
