134d0edc42af603ef29ddf6d62a0d09a2913bf4f
mizuki0220 <em9220me.i@gmail.com>
2025-03-12 14:58:51 +0000

    admin/注文履歴詳細　途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,16 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find_by(id: params[:id])
   end
 
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      flash[:notice] = "個数を変更しました"
+      redirect_to admin_order_path
+    else
+      flash[:alert] = "正しい個数を入力してください"
+      render :show
+    end
   end
 end
