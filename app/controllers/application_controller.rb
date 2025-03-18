81821f89034035830937d73f90c1efa54835bc0a
mizuki0220 <em9220me.i@gmail.com>
2025-03-18 02:42:09 +0000

    アクセス制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_admin!
+  # before_action :authenticate_admin!
 
   protected
 
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:first_name_kana,:last_name,:last_name_kana,:postal_code,:address,:telephone_number,:telephone_number])
   end
 
-  def authenticate_admin!
-    unless current_admin
-      redirect_to new_admin_session_path, alert: "ログインしてください"
-    end
-  end
+  # def authenticate_admin!
+  #   unless current_admin
+  #     redirect_to new_admin_session_path, alert: "ログインしてください"
+  #   end
+  # end
 end
